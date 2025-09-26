bf6b3a8f0a900a759e278bf260747adcc3ceb5bf
Kjagan1907 <Kjagan304@gmail.com>
2025-09-27 00:40:25 +0200

    deletion of known words from the file

--- german_flashcards.py
+++ german_flashcards.py
@@ -1,33 +1,49 @@
 import tkinter as tk
-import numpy as np
+import random
 import pandas as pd
+import csv
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- FLASHCARD LOGIC ------------------------------- #
-df = pd.read_csv("frequent_germanwords.csv")
-dictionary = df.to_dict(orient="records")
+try:
+    data = pd.read_csv("unknown_words.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("frequent_germanwords.csv")
+    dictionary = original_data.to_dict(orient="records")
+else:
+    dictionary = data.to_dict(orient="records")
+
 
 def flip_card():
     canvas.itemconfig(card_front, image=card_back_img)
-    canvas.itemconfig(lang, text="English")
-    canvas.itemconfig(german_card)
+    canvas.itemconfig(lang, text="English", fill="white")
+    canvas.itemconfig(german_card, text=flashcard["English"], fill="white")
+
 
 
 def card():
-    flashcard = np.random.choice(dictionary)
+    global flashcard, timer
+    window.after_cancel(timer)
+    flashcard = random.choice(dictionary)
     canvas.itemconfig(card_front, image=card_front_img)
-    canvas.itemconfig(lang, text="German")
-    canvas.itemconfig(german_card, text=flashcard["German"])
-    window.after(3000, flip_card)
-    #
-    
+    canvas.itemconfig(lang, text="German", fill="black")
+    canvas.itemconfig(german_card, text=flashcard["German"], fill="black")
+    timer = window.after(3000, flip_card)
+
+def known_word():
+    dictionary.remove(flashcard)
+    print(len(dictionary))
+    new_data = pd.DataFrame(dictionary)
+    new_data.to_csv("unknown_words.csv", index=False)
+    card()    
 
 
 # ---------------------------- GUI -------------------------------- #
 window = tk.Tk()
 window.title("German Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+timer = window.after(3000, card)
 
 canvas = tk.Canvas(width=800, height=526)
 card_front_img = tk.PhotoImage(file="card_front.png")   
@@ -42,7 +58,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 right_image = tk.PhotoImage(file="right.png")
 wrong_image = tk.PhotoImage(file="wrong.png")
-right_button = tk.Button(image=right_image, highlightthickness=0, command=card)
+right_button = tk.Button(image=right_image, highlightthickness=0, command=known_word)
 right_button.grid(row=1, column=1)
 wrong_button = tk.Button(image=wrong_image, highlightthickness=0, command=card)
 wrong_button.grid(row=1, column=0)
